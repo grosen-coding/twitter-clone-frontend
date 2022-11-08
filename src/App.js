a4969e491f1a47dcd85d2cfcf2f69a081b8279e3
k9paws <candogservices@gmail.com>
2022-11-07 21:21:49 -0500

    successfully accessed API test data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
 import "./App.css";
 import Tweet from "./Tweet";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [tweets, setTweets] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://coursera-twitter-api.herokuapp.com/tweets")
+      .then((res) => {
+        const tweetData = res.data;
+
+        console.log(tweetData);
+        setTweets(tweetData);
+        console.log(res);
+        console.log("The data was retrieved");
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log("Error Message is working");
+      });
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Hello, World</h1>
-      <Tweet text="Text passed down" />
+    <div>
+      <Tweet tweets={tweets} />
     </div>
   );
 }
--- src/Tweet.js
+++ src/Tweet.js
@@ -1,30 +1,13 @@
-import React, { useState } from "react";
+import React from "react";
 
-function Tweet({ text }) {
-  const newText = text;
-  const [count, setCount] = useState(0);
-
-  const initialCount = count;
-
-  const increment = () => {
-    setCount(initialCount + 1);
-  };
-
-  const decrement = () => {
-    setCount(initialCount - 1);
-  };
-
-  const reset = () => {
-    setCount(count);
-  };
-
+function Tweet({ tweets }) {
   return (
     <div>
-      <h1>{newText.toLowerCase()}</h1>
-      <h4>Counter: {initialCount}</h4>
-      <button onClick={increment}>+</button>
-      <button onClick={decrement}>-</button>
-      <button onClick={reset}>reset</button>
+      <ul>
+        {tweets.map((tweet) => (
+          <li key={tweet.id}>{tweet.text}</li>
+        ))}
+      </ul>
     </div>
   );
 }
